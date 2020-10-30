75ceaf6aec1e951c1c7e2a18913b9cdf138b8d9d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-30 14:18:42 +0900

    [Add] stdin ver of main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
 int main(int ac, char **av)
 {
 	int fd = open(av[1], O_RDONLY);
@@ -12,5 +13,17 @@ int main(int ac, char **av)
 		printf("%s\n", line);
 	printf("%s\n", line);
 	close(fd);
+	return (0);
+}
+
+
+//stdin ver
+int main(int ac, char **av)
+{
+	int fd = 0;
+	char *line;
+	while (get_next_line(fd, &line))
+		printf("%s\n", line);
+	printf("%s\n", line);
 	return (0);
 }

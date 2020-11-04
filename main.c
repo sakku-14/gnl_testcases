16f54316369de2475848e1721d8f42e49751e31d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-11-05 06:39:48 +0900

    [Improve] some

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <string.h>
 
-
+/*
 int main(int ac, char **av)
 {
 	int fd = open(av[1], O_RDONLY);
@@ -20,6 +20,50 @@ int main(int ac, char **av)
 	close(fd);
 	ac = 0;
 	return (0);
+}
+*/
+
+//some av ver totyuuuuuuuuuuuuuuuuuuuuuu
+int main(int ac, char **av)
+{
+	int		fd[MAX_FD];
+	char	*line;
+	int		i, flag = 0;
+	int		ret;
+
+	while(i + 1 < ac)
+	{
+		fd[i] = open(av[i + 1], O_RDONLY);
+		i++;
+	}
+	i = 0;
+	while(1)
+	{
+		while(i + 1 < ac)
+		{
+			if((ret = get_next_line(fd[i], &line)) == -1)
+				flag = 1;
+			if (flag == 0)
+			{
+				printf("fd%d:%d:%s\n", i, ret, line);
+				free(line);
+				i++;
+			}
+			else
+			{
+				printf("fd%d:%d:...error\n", i, ret);
+			}
+			flag = 0;
+		}
+		i = 0;
+	}
+	i = 0;
+	while (i + 1 < ac)
+	{
+		close(fd[i]);
+		i++;
+	}
+		return (0);
 }
 
 
